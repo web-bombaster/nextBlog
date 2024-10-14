0ce40834ddfe504cb11cac580d16ace4a0a649d5
Yurij Moshatin <y.moshatin@rbru-test.ru>
2024-10-14 23:58:18 +0300

    save

--- src/components/pages/blog/BlogCategory/BlogCategory.tsx
+++ src/components/pages/blog/BlogCategory/BlogCategory.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "./blogCategory.module.scss";
 import { blogPostsArr as posts } from "./blogPostsArr";
 
-interface BlogPost {
+interface IBlogPost {
 	id: string;
 	title: string;
 	descr: string;
@@ -11,19 +11,9 @@ interface BlogPost {
 	img: string;
 }
 
-interface BlogPosts {
-	items: BlogPost[];
+interface IBlogPosts {
+	items: IBlogPost[];
 }
-
-const fetchData = async (): Promise<BlogPosts> => {
-	return fetch("./blogPostsArr.js", {
-		method: "GET",
-		headers: {
-			//   'X-API-KEY': '07ca675a-db71-4e5b-8119-5618275b2fab',
-			"Content-Type": "application/json",
-		},
-	}).then((response) => response.json());
-};
 
 const BlogCategory = () => {
 	return (
@@ -46,25 +36,6 @@ const BlogCategory = () => {
 					<button className={styles.more}>Подробнее</button>
 				</Link>
 			))}
-
-			{/* {posts.map((postItem) => (
-				<Link
-					key={postItem.id}
-					href={postItem.link}
-					className={styles.item}
-				>
-					<Image
-						className={styles.img}
-						src={postItem.img}
-						width="780"
-						height="440"
-						alt=""
-					/>
-					<h2 className={styles.title}>{postItem.title}</h2>
-					<p className={styles.descr}>{postItem.descr}</p>
-					<button className={styles.more}>Подробнее</button>
-				</Link>
-			))} */}
 		</div>
 	);
 };
